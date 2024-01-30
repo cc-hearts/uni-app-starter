ea3f8ab64e7ceb2f1dd79dc82056886b369240b8
carl-chen <ccheartszzz@gmail.com>
2024-01-31 02:30:45 +0800

    chore: fix dep

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,12 @@
+
 module.exports = {
-  extends: '@cc-heart/eslint-config',
+  extends: '@cc-heart/eslint-config/vue3-typescript-eslint',
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
     ecmaVersion: 'laster',
+  },
+  "rules":{
+    "vue/component-name-in-template-casing":"off"
   }
 }
