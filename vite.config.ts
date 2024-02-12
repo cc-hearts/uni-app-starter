1b7b421e8d7114330653cde39ff12151fd2d027f
Carl Chen <ccheartszzz@gmail.com>
2024-02-12 21:46:43 +0800

    chore: add uni-pages plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
 import uni from '@dcloudio/vite-plugin-uni'
 import UnoCSS from 'unocss/vite'
+import UniPages from '@uni-helper/vite-plugin-uni-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [uni(), UnoCSS()],
+  plugins: [uni(), UnoCSS(), UniPages()],
   define: {
     'process.env': {
       UNI_MODES: process.env.MODE_ENV || 'development',
