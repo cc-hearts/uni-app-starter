c52ee39164e457d5f6e9844b1494ab2583ffded8
Carl Chen <ccheartszzz@gmail.com>
2024-04-10 21:14:16 +0800

    perf: add uni pages plugin config

---
+++ page.config.ts
@@ -0,0 +1,12 @@
+// pages.config.ts
+import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
+
+export default defineUniPages({
+  easycom: {
+    autoscan: true,
+    custom: {
+      // uni-ui 规则如下配置
+      '^uni-(.*)': '@dcloudio/uni-ui/lib/uni-$1/uni-$1.vue',
+    },
+  },
+})
