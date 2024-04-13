4014cd5649192dfc09b52422bffc570d5f87ce99
Carl Chen <ccheartszzz@gmail.com>
2024-04-13 23:25:08 +0800

    perf: optimize

--- src/guard/router.guard.ts
+++ src/guard/router.guard.ts
@@ -4,6 +4,6 @@ import type { RouterPluginOptions } from '@/modules/routers'
 export const beforeRouterGuard: RouterPluginOptions['guard'] = (from, to) => {
   // TODO
   if (__IS_DEV__) {
-    console.log(from, to)
+    console.log('before router guard:', from, to)
   }
 }
