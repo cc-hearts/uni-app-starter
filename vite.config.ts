9dbd46d72f27910c7629df52b09692aa0e3d4999
carl-chen <ccheartszzz@gmail.com>
2024-05-16 12:40:57 +0800

    chore: add auto import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import { resolve } from 'node:path'
 import uni from '@dcloudio/vite-plugin-uni'
 import UnoCSS from 'unocss/vite'
 import UniPages from '@uni-helper/vite-plugin-uni-pages'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [uni(), UnoCSS(), UniPages()],
+  plugins: [
+    uni(),
+    UnoCSS(),
+    UniPages(),
+    AutoImport({
+      imports: ['vue'],
+    }),
+  ],
   define: {
     'process.env': {
       UNI_MODES: process.env.MODE_ENV || 'development',
