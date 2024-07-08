f7b074627bd752fb948d950189fe1584bdf38481
carl-chen <ccheartszzz@gmail.com>
2024-07-08 01:14:36 -0300

    perf: add types

--- src/hooks/use-current-instance.ts
+++ src/hooks/use-current-instance.ts
@@ -1,5 +1,7 @@
+import type { GetArrayChildItem } from '@cc-heart/utils/helper'
+
 export function useCurrentInstance() {
   const [pages] = getCurrentPages().slice(-1)
   if (!pages) return null
-  return pages
+  return pages as GetArrayChildItem<ReturnType<typeof getCurrentPages>> & { options: Record<string, string> }
 }
