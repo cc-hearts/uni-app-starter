df648d3a6780531b9a52d5ddf526331fd61a1396
carl-chen <ccheartszzz@gmail.com>
2024-07-15 13:25:09 +0800

    chore: upgrade deps version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,6 @@
 import { vue, typescript } from '@cc-heart/eslint-config'
 
 export default [
-  ...vue({ typescript: true, parserOptionsOverrides: { project: false } }),
-  ...typescript({
-    parserOptionsOverrides: {
-      project: true
-    }
-  })
+  ...vue({ typescript: true}),
+  ...typescript()
 ]
